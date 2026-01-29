aaba6e3ded742a3b565b2db79b466e6028613dae
Yiming Su <yiming34@illinois.edu>
2026-01-29 15:42:21 -0600

    installs node too

--- brew.sh
+++ brew.sh
@@ -9,3 +9,18 @@ eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 sudo apt install build-essential -y
 brew install gcc kubectl helm fd fzf rg
+
+# Download and install nvm:
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+
+# in lieu of restarting the shell
+\. "$HOME/.nvm/nvm.sh"
+
+# Download and install Node.js:
+nvm install 24
+
+# Verify the Node.js version:
+node -v # Should print "v24.13.0".
+
+# Verify npm version:
+npm -v # Should print "11.6.2".
